78c4b28dcf6e091d7bc686d81f9fe8b5b4d1bec4
shrutileena <shrutileena@gmail.com>
2020-08-15 06:57:49 +0530

    committed

---
+++ Square.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				System.out.print("*"+ " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
