1e3cf633582b4738107d82d9e01e66e1ce801a23
Shruti Agarwal <50164469+shrutileena@users.noreply.github.com>
2020-08-18 02:54:21 +0000

    Loops

---
+++ SeriesSum.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class SeriesSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		float result = 0;
+		
+		for (float i=1; i<=n; i++) {
+			result += 1/i;
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
