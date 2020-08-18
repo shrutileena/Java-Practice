95597c9e23e48f6380185e7e437cfcb147486820
Shruti Agarwal <50164469+shrutileena@users.noreply.github.com>
2020-08-18 02:53:46 +0000

    Loops

---
+++ Prime.java
@@ -0,0 +1,34 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		boolean isPrime = true;
+		
+		if (n < 2)
+			isPrime = false;
+		
+		for (int i=2; i<n; i++) {
+			
+			if (n % i == 0) {
+				isPrime = false;
+				break;
+			}
+			
+		}
+		
+		if (isPrime == true)
+			System.out.println("It is a prime number");
+		else
+			System.out.println("It is not a prime number");
+		
+	}
+
+}
