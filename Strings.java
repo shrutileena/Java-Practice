8bb992d6d088f2a171ab8618f4cc20d3c4c449e4
Shruti Agarwal <50164469+shrutileena@users.noreply.github.com>
2020-08-18 02:57:50 +0000

    Strings

---
+++ Strings.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String name = "Shruti";;
+		String anotherName = new String("Abhi");
+		System.out.println(name);
+	}
+}
